fdf3d1576e89ee74a9870bc168dfada29e237d36
si-shakil <sishakil850@gmail.com>
2022-04-04 20:00:51 +0600

    about section updated

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <div className="container mt-5">
       <h6>Address:</h6>
       <address>
-        ©1997-2022 Barnes & Noble Booksellers, Inc. 33 East 17th Street, New
+        ©1997-2022 Hossain & Sons Booksellers, Inc. 33 East 17th Street, New
         York, NY 10003
       </address>
     </div>
