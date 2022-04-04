5866bc8d0c3c03e1fb74d10b36c1f3c75ebc3929
si-shakil <sishakil850@gmail.com>
2022-04-04 19:56:55 +0600

    completed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <p>This is about section.</p>
+    <div className="container mt-5">
+      <h6>Address:</h6>
+      <address>
+        ©1997-2022 Barnes & Noble Booksellers, Inc. 33 East 17th Street, New
+        York, NY 10003
+      </address>
     </div>
   );
 };
